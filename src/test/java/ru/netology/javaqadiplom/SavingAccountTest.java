d5e7afb27fc07bc0179f27f94cb6a2fa0863917e
Krasnikov <krastist@yandex.ru>
2023-06-03 15:40:16 +0500

    Add Tests Branch Saving

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -16,6 +16,103 @@ public class SavingAccountTest {
 
         account.add(3_000);
 
-        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+        Assertions.assertEquals(2_000 + 3_000, account.getBalance());  // Тест не проходит
+    }                                                                          // В 73 строке кода ошибка
+
+    @Test
+    public void shouldAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_001);
+
+        Assertions.assertEquals(2_000, account.getBalance());   // Тест проходит
     }
+
+    @Test
+    public void shouldAddEqualToMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(8_000);
+
+        Assertions.assertEquals(10_000, account.getBalance());  // Тест не проходит
+    }                                                                   // В 73 строке кода ошибка
+
+    @Test
+    public void payTest() {
+        SavingAccount account = new SavingAccount(100, 10, 200, 4);
+
+        account.pay(90);
+
+        Assertions.assertEquals(10, account.getBalance());  // Тест проходит
+    }
+
+    @Test
+    public void payLessMinTest() {
+        SavingAccount account = new SavingAccount(100, 10, 200, 4);
+
+        account.pay(91);
+
+        Assertions.assertEquals(100, account.getBalance());     // Тест не проходит
+    }                                                                   // В 48 строке кода ошибка
+
+    @Test
+    public void payMoreBalanceTest() {
+        SavingAccount account = new SavingAccount(100, 10, 200, 4);
+
+        account.pay(101);
+
+        Assertions.assertEquals(100, account.getBalance());     // Тест не проходит
+    }                                                                   // В 48 строке кода ошибка
+
+    @Test
+    public void yearChangeTest() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                10_000,
+                15
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(30, account.getBalance());   // Тест не проходит
+    }                                                                // В 89 строке кода ошибка
+
+    @Test
+    public void yearChangeNegativePercentTest() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                10_000,
+                -5
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(0, account.getBalance());   // Тест не проходит
+    }                                                                // В 89 строке кода ошибка
+
+    @Test
+    public void yearChangeZeroPercentTest() {
+        SavingAccount account = new SavingAccount(
+                200,
+                100,
+                10_000,
+                0
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(0, account.getBalance());   // Тест не проходит
+    }                                                                // В 89 строке кода ошибка
 }
